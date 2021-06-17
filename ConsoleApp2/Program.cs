de0075ff8b4ccd720d2591c54a90140a6c933264
szKotya <moneymilhe1250@gmail.com>
2021-06-17 06:52:24 +0300

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = WriteData();
+            b = WriteData();
+            c = WriteData();
+
+            Console.WriteLine("Сумма. = " + ((int)(a + b + c)));
+            Console.WriteLine("Умножение. = " + ((int)(a * b * c)));
+        }
+        static int WriteData()
+        {
+            string temp;
+
+            while (true)
+            {
+                Console.WriteLine("Введите значение");
+                temp = Console.ReadLine();
+                try
+                {
+                    return int.Parse(temp);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не правильный тип данных");
+                    continue;
+                }
+            }
+        }
+    }
+}
