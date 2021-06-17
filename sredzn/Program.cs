bca6e56ffdc9c91f2582412a1381b75e5d3c222a
szKotya <moneymilhe1250@gmail.com>
2021-06-17 06:57:01 +0300

    add sredzn

---
+++ sredzn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = WriteData();
+            b = WriteData();
+            c = WriteData();
+
+            Console.WriteLine("СредАрф. = " + ((double)((a + b + c)/3)));
+        }
+        static int WriteData()
+        {
+            string temp;
+
+            while (true)
+            {
+                Console.WriteLine("Введите значение");
+                temp = Console.ReadLine();
+                try
+                {
+                    return int.Parse(temp);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не правильный тип данных");
+                    continue;
+                }
+            }
+        }
+    }
+}
